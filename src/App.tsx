fb71ee2da5fd71f6beca36295acef29dbc35161f
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-02-04 10:37:17 -0300

    trying to work with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import LateralMenu from './components/LateralMenu';
+import ContentProject from './components/projects/ContentProject';
 
 function App() {
   return (
@@ -11,15 +13,22 @@ function App() {
         <Header />
       </header>
       <main className='container-main'>
-        <div className='lateral-menu'>
-          <LateralMenu />
-        </div>
-        <div className='content'>
-          ...
-        </div>
+        <Routes>
+          <Route path='/' element={
+            <>
+              <div className='lateral-menu'>
+                <LateralMenu />
+              </div>
+              <div className='content'>
+                <ContentProject />
+              </div>
+            </>
+          } />
+        </Routes>
       </main>
     </div>
-  );
+
+  )
 }
 
 export default App;
